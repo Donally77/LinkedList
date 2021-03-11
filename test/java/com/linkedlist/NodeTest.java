f0211cd910aa6bdca68179d74d4a507a247248db
Donally77 <donallypatel0707@gmail.com>
2021-03-11 11:04:14 +0530

    adding node to list

--- test/java/com/linkedlist/NodeTest.java
+++ test/java/com/linkedlist/NodeTest.java
@@ -2,6 +2,7 @@ package com.linkedlist;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NodeTest {
@@ -15,5 +16,22 @@ public class NodeTest {
         boolean result = firstNode.getNext().equals(secondNode) && secondNode.getNext().equals(thirdNode);
         assertTrue(result);
     }
+    //uc2 for adding nodes in list
+    @Test
+    public void testforAdd() {
+        System.out.println("This test is for adding node in the list.");
+        Node<Integer> firstNode = new Node<>(70);
+        Node<Integer> secondNode = new Node<>(30);
+        Node<Integer> thirdNode = new Node<>(56);
+        LinkedList linkedlist = new LinkedList();
+        linkedlist.add(firstNode);
+        linkedlist.add(secondNode);
+        linkedlist.add(thirdNode);
+        linkedlist.print();
+        boolean result = linkedlist.head.equals(thirdNode) && linkedlist.head.getNext().equals(secondNode)
+                && linkedlist.tail.equals(firstNode);
+        assertEquals(true, result);
+    }
+
 
 }
