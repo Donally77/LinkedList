fdc07106ecb2050f9c209df304a302bd0de7f663
Donally77 <donallypatel0707@gmail.com>
2021-03-11 10:32:11 +0530

    welcome message

---
+++ main/java/com/linkedlist/LinkedList.java
@@ -0,0 +1,8 @@
+package com.linkedlist;
+
+public class LinkedList {
+    public static void main(String[] args){
+        //Mater welcome message
+        System.out.println("....Welcome to LinkedList Program....");
+    }
+}
